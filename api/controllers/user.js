7ae0a12c5f303e59bc59a5319f3d050b7b100ffc
Darren Benn <darren.benn@digitalcontact.co.uk>
2016-12-21 17:21:31 +0000

    Initial commit

---
+++ api/controllers/user.js
@@ -0,0 +1,57 @@
+'use strict';
+    
+var db = require('../../config/db')();
+
+module.exports = {getAll, save, get, update, deleteUser};
+
+function getAll(req, res, next) {
+  res.json({ 
+    users: db.find()
+  });
+}
+
+function save(req, res, next) {
+    var newUser = db.save(req.body);
+    if (newUser) {
+      res.status(201).json(newUser);
+    } else {
+      res.status(202).send();
+    }
+}
+
+function get(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    var user = db.find(id);
+
+    if(user) {
+        res.json(user);
+    } else {
+        res.status(204).json({ message: "User could not be found"});
+    }       
+}
+
+function update(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    var user = req.body;
+    var updatedUser = db.update(id, user);
+
+    if(updatedUser) {
+        res.json(updatedUser);
+    } else {
+        res.status(204).json({ message: "User could not be found"});
+    }
+
+}
+
+function deleteUser(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+
+    if(db.remove(id)) {
+        res.json({
+          success: 1, description: "User has been deleted!"
+        });
+    } else{ 
+        res.status(204).json({ message: "User could not be found"});
+    }
+
+}
---
+++ config/db.js
@@ -0,0 +1,53 @@
+'use strict;'
+
+var crypto = require('crypto');
+
+module.exports = function() {
+    return {
+        userList : [],
+
+        save(user) {
+            user.id = crypto.randomBytes(20).toString('hex');
+            user.created = new Date();
+            this.userList.push(user);
+
+            return user;           
+        },
+        
+        find(id) {
+            if(id) {
+                return this.userList.find(element => {
+                        return element.id === id;
+                    }); 
+            } else {
+                return this.userList;
+            }
+        },
+
+        remove(id) {
+            var found = 0;
+            this.userList = this.userList.filter(element => {
+                    if(element.id === id) {
+                        found = 1;
+                    } else {
+                        return element.id !== id;
+                    }
+                });
+            return found;           
+        },
+
+        update(id, user) {
+            var userIndex = this.userList.findIndex(element => {
+                return element.id === id;
+            });
+            if(userIndex !== -1) {
+                this.userList[userIndex].email = user.email;
+                this.userList[userIndex].forename = user.forename;
+                this.userList[userIndex].surname = user.surname;
+                this.userList[userIndex].updated = new Date();
+
+                return this.userList[userIndex];
+            }
+        }       
+    }
+};  
