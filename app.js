c844867fb51d3917f41d0905675153d27cb69925
Darren Benn <darren.benn@digitalcontact.co.uk>
2016-12-23 17:38:49 +0000

    Scaffold user management UI

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var express = require('express');
 var app = require('express')();
+var path = require('path');
 
 module.exports = app; // for testing
 
@@ -12,9 +14,15 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
 	if (err) { throw err; }
 
+	app.get('/', function(req, res, next) {
+	 	res.sendFile(path.join(__dirname + '/app/index.html'));
+	});
+	app.use("/styles", express.static(__dirname + '/app/css'));
+	app.use("/scripts", express.static(__dirname + '/app/js'));
+
 	// Serve the Swagger documents and Swagger UI
 	app.use(swaggerExpress.runner.swaggerTools.swaggerUi());
- 
+
 	// install middleware
 	swaggerExpress.register(app);
 
